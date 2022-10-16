ab2e5bb152d9f17ba250775892ecf173cb47f6b2
Azard <azardf4yy@gmail.com>
2022-10-16 21:54:08 +0800

    i18n compile

--- src/components/PositionCard/index.tsx
+++ src/components/PositionCard/index.tsx
@@ -132,7 +132,7 @@ export function MinimalPositionCard({ pair, border }: PositionCardProps) {
               ⭐️
             </span>{' '}
             <Trans>
-              By adding liquidity you&apos;ll earn 0.3% of all trades on this pair proportional to your share of the
+              By adding liquidity you&apos;ll earn 0.25% of all trades on this pair proportional to your share of the
               pool. Fees are added to the pool, accrue in real time and can be claimed by withdrawing your liquidity.
             </Trans>{' '}
           </ThemedText.DeprecatedSubHeader>
