21ef33dcd8fb8567217e1396b06dee8b7e2e4e97
Azard <azardf4yy@gmail.com>
2022-10-22 23:57:48 +0800

    update coinbase

--- src/constants/coinbases.ts
+++ src/constants/coinbases.ts
@@ -9,8 +9,8 @@ export const COIN_BASES = {
     APTOS_CoinInfo[
       '0x84d7aeef42d38a5ffc3ccef853e1b82e4958659d16a7de736a29c55fbbeb0114::staked_aptos_coin::StakedAptosCoin'
     ],
-    APTOS_CoinInfo['0x881ac202b1f1e6ad4efcff7a1d0579411533f2502417a19211cfc49751ddb5f4::coin::MOJO'],
-    APTOS_CoinInfo['0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::BusdCoin'],
+    APTOS_CoinInfo['0xd11107bdf0d6d7040c6c0bfbdecb6545191fdf13e8d8d259952f53e1713f61b5::staked_coin::StakedAptos'],
+    APTOS_CoinInfo['0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDT'],
   ],
   [SupportedChainId.APTOS_TESTNET]: [],
   [SupportedChainId.APTOS_DEVNET]: [],
